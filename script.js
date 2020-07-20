f01e51e9c61858305f46f940a5edd4c2532a502a
Juan Mireles <jmireles12@gmail.com>
2020-07-19 22:41:02 -0500

    last changes

---
+++ script.js
@@ -0,0 +1,29 @@
+function getDogImages(breed) {
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(response => response.json())
+    .then(responseJson => displayResult(responseJson))
+    .catch(error => alert(responseJson.message));
+}
+
+function displayResult(responseJson) {
+    console.log(responseJson)
+    if (responseJson.status === "error") {
+        alert(responseJson.message)
+    }
+    $('.result-img').replaceWith(`<img src="${responseJson.message}" class="result-img">`)
+    $('.result').removeClass('hidden');
+}
+
+function watchForm() {
+    $('form').submit(event => {
+        let name = $(breed).val()
+        console.log('submit');
+        event.preventDefault();
+        getDogImages(name);
+    });
+}
+
+$(function () {
+    console.log('App loaded! Waiting for submit!');
+    watchForm();
+});
